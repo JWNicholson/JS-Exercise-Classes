513d46d55f312fbe761c0821b98ec90c37c712bb
JW Nicholson <john.nicholson812@gmail.com>
2020-01-30 16:58:13 -0500

    starting on Student extends

--- index.js
+++ index.js
@@ -158,8 +158,8 @@ class Instructor extends Lambdasian{
     return `Today we are learning about ${subject}`;
   }
 
-  grade(student, subject){
-
+  grade(student,subject){
+return `${student} receives a perfect score on ${subject}`;
   }
 
 }
@@ -179,7 +179,7 @@ class Instructor extends Lambdasian{
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
 
 }
 
